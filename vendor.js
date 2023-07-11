d2d238012c4fb737163527b3742b2d47eb54e81f
jaysen <jaysenhdezlopez@gmail.com>
2023-07-11 10:19:46 -0400

    last commit before adding nodejs

--- vendor.js
+++ vendor.js
@@ -3,7 +3,7 @@ let tiposDePeso = [];
 let gruposMusculares = [];
 
 // Leer json
-fetch('data.json')
+fetch('data_sorted.json')
     .then(response => response.json())
     .then(data => {
         gruposMusculares = data.GruposMusculares;
