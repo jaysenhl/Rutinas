5fffc88a2499ce0339431418151450a5c050f7c1
jaysen <jaysenhdezlopez@gmail.com>
2023-07-11 13:29:10 -0400

    Removed enviroment variables to before

--- Javascript/firebaseConfig.js
+++ Javascript/firebaseConfig.js
@@ -2,13 +2,13 @@
 
 // Firebase configuration
 var firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyB7pUSAuL3_DWGfOfaEBAmjUhrVirTGc7Q",
+    authDomain: "misrutinas-65775.firebaseapp.com",
+    projectId: "misrutinas-65775",
+    storageBucket: "misrutinas-65775.appspot.com",
+    messagingSenderId: "240378783112",
+    appId: "1:240378783112:web:0084e63f1d6a17212e063d",
+    measurementId: "G-MXT6Q33B2W"
   };
   
   // Initialize Firebase
